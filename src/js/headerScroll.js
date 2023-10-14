1c16c93e162170ccd51307df3655fbd1f74c69bc
teorver <Don1945@mail.ru>
2023-10-14 12:08:46 +0200

    add: scrollable header and backgroundColor change

--- src/js/headerScroll.js
+++ src/js/headerScroll.js
@@ -1,20 +1,8 @@
-let lastScroll = 0;
-const header = document.querySelector('.header-section');
-const main = document.getElementsByClassName('main');
-
-// const scrollPosition = () => window.pageY || document.documentElement.scrollTo();
-
-window.addEventListener('scroll', () => {
-    let scrollPosition = () => window.pageY;
-    let containHide = () => header.classList.contains('hide');
-
-    if (scrollPosition > lastScroll && !containHide) {
-        header.classList.add('hide');
-        header.style.backgroundColor = '#2c3154'
-    } else if (scrollPosition < lastScroll && containHide) {
-        header.classList.remove('hide');
-        header.style.backgroundColor = '#2c3154'
-    }
-
-    lastScroll = scrollPosition();
+$(function () {
+    $(document).scroll(function () {
+        let $header = $("header");
+        let $header__container = $(".header__container");
+        $header.toggleClass('scrolled', $(this).scrollTop() > $header.height());
+        $header__container.toggleClass('scrolled-border', $(this).scrollTop() > $header.height());
+    });
 });
