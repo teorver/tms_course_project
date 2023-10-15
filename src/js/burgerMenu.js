9b82c85339bde8c45d95ac1f367309d768da7e91
teorver <Don1945@mail.ru>
2023-10-15 22:03:32 +0200

    update: replace jQuery with vanilla JS

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,5 +1,14 @@
-$(document).ready(function(){
-    $('.burger-menu').click(function(){
-        $('.navigation').toggleClass('active');
+// $(document).ready(function(){
+//     $('.burger-menu').click(function(){
+//         $('.navigation').toggleClass('active');
+//     });
+// });
+
+document.addEventListener('DOMContentLoaded', function() {
+    let burgerMenu = document.querySelector('.burger-menu');
+    let navigation = document.querySelector('.navigation');
+
+    burgerMenu.addEventListener('click', function() {
+        navigation.classList.toggle('active');
     });
 });
--- src/js/headerScroll.js
+++ src/js/headerScroll.js
@@ -1,8 +1,26 @@
-$(function () {
-    $(document).scroll(function () {
-        let $header = $("header");
-        let $header__container = $(".header__container");
-        $header.toggleClass('scrolled', $(this).scrollTop() > $header.height());
-        $header__container.toggleClass('scrolled-border', $(this).scrollTop() > $header.height());
+// $(function () {
+//     $(document).scroll(function () {
+//         let $header = $("header");
+//         let $header__container = $(".header__container");
+//         $header.toggleClass('scrolled', $(this).scrollTop() > $header.height());
+//         $header__container.toggleClass('scrolled-border', $(this).scrollTop() > $header.height());
+//     });
+// });
+
+document.addEventListener('DOMContentLoaded', function () {
+    let header = document.querySelector('header');
+    let headerContainer = document.querySelector('.header__container');
+
+    document.addEventListener('scroll', function () {
+        let scrollPosition = window.scrollY;
+        let headerHeight = header.offsetHeight;
+
+        if (scrollPosition > headerHeight) {
+            header.classList.add('scrolled');
+            headerContainer.classList.add('scrolled-border');
+        } else {
+            header.classList.remove('scrolled');
+            headerContainer.classList.remove('scrolled-border');
+        }
     });
 });
