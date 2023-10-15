959b6ca45c73b5c4e87c7d8e1c39b8b050e74f7a
teorver <Don1945@mail.ru>
2023-10-15 22:04:15 +0200

    Updates

---
+++ index.4e6f74aa.js
@@ -0,0 +1,6 @@
+// $(document).ready(function(){
+//     $('.burger-menu').click(function(){
+//         $('.navigation').toggleClass('active');
+//     });
+// });
+document.addEventListener("DOMContentLoaded",function(){let e=document.querySelector(".burger-menu"),t=document.querySelector(".navigation");e.addEventListener("click",function(){t.classList.toggle("active")})});
