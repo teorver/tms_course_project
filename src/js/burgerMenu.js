266d59786a574879cc292fd6434b9b01bdb40cc0
teorver <Don1945@mail.ru>
2023-10-16 00:10:04 +0200

    refactor

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     let burgerMenu = document.querySelector('.burger-menu');
     let navigation = document.querySelector('.navigation');
+    // let headerContainer = document.querySelector('.header__container');
 
     burgerMenu.addEventListener('click', function() {
         navigation.classList.toggle('active');
         burgerMenu.classList.toggle('open-menu');
+        // headerContainer.classList.toggle('img-change');
     });
 });
